7ae36f161fbfaf01f8db3a606d4030a9b0969722
sanskarfc <sanskarfc@gmail.com>
2024-04-29 20:44:47 +0530

    included freq and location data as well

--- search_engine.cpp
+++ search_engine.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <string>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -15,25 +16,47 @@ class SearchEngine {
 public:
   void indexDocument(const string &index_name, const string &text) {
     vector<string> tokens = tokenize(text);
+    int loc = 0;
     for (const auto &token : tokens) {
+      freq_loc[token][index_name].push_back(loc);
       inverted_index_[token].push_back(index_name);
+      loc++;
     }
   }
 
   vector<string> matchQuery(const string &query) {
     vector<string> tokens = tokenize(query);
+    map<string, int> freq;
     set<string> matching_indices;
     for (const auto &token : tokens) {
       if (inverted_index_.find(token) != inverted_index_.end()) {
         for (const auto &index : inverted_index_[token]) {
           matching_indices.insert(index);
+          freq[index]++;
         }
       }
     }
+
+    for (const auto &token : tokens) {
+      if (freq_loc.find(token) != freq_loc.end()) { // token is present
+        cout << "Token: " << token << endl;
+        for (auto p : freq_loc[token]) {
+          cout << "Document: " << p.first << endl;
+          cout << "Location in Doument: ";
+          for (auto elem : p.second) {
+            cout << elem << ", ";
+          }
+          cout << endl;
+        }
+        cout << endl;
+      }
+    }
+
     return vector<string>(matching_indices.begin(), matching_indices.end());
   }
 
 private:
+  map<string, map<string, vector<int>>> freq_loc;
   unordered_map<string, vector<string>> inverted_index_;
 
   vector<string> tokenize(const string &text) {
@@ -65,7 +88,7 @@ int main() {
       break;
     } else if (command == "index") {
       string index_name, document_text;
-      cout << "Enter index name: ";
+      cout << "Enter doc name: ";
       cin >> index_name;
       cin.ignore();
       cout << "Enter document text: ";
